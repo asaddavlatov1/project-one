b6b61360591f5b7e4781d121c3df7badaa697da3
asaddavlatov1 <asaddablatov1@gmail.com>
2022-03-02 17:21:49 +0500

    first commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,7 @@
           <meta name="viewport" content="width=device-width, initial-scale=1.0">
           <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
           <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-          <title>Document</title>
+          <title>Dokument</title>
 </head>
 <body>
           <div class="container">
@@ -42,11 +42,7 @@
                               
                                         <div class="form-group">
                                         <label for="exampleFormControlTextarea1">Comment</label>
-                                        <textarea class="form-control" id="comment" rows="3" name="comment"></textarea>
-                                        </div>
-                                        <br>
-
-                                        <button type="submit" class="btn btn-success">Success</button>
+                                        <textarea class="form-control" id="comment" rows="3" name="comment"></textarea>                    <button type="submit" class="btn btn-success">Success</button>
                               </form>
                               </div>
                     </div>
